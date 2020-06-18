600e3a996e9402231f336dc6119f12531a2f6dbd
efefurkankarakaya <efkdev@outlook.com>
2020-06-18 20:14:41 +0300

    minor code refactoring

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const edit_buttons = document.getElementsByClassName("edit");
 
-for (let i = 0; i < edit_buttons.length; i++){ edit_buttons[i].addEventListener("click", call_frame); }
-
-function call_frame(){
-    const edit = new Edit();
-    edit.create_frame();
-}
+// creates the frame for each todos if the click event happens
+for (let i = 0; i < edit_buttons.length; i++){ edit_buttons[i].addEventListener("click", 
+new Edit().create_frame) }
